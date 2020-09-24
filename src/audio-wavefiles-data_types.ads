b6133a8028f15dc66c0907fa134f6119b3186a2c
gusthoff <gusthoff@gusthoff.com>
2020-09-24 13:09:05 +0200

    Adding Wavefiles.Data_Types package
    Package contains fixed/floating-point type definitions.

---
+++ src/audio-wavefiles-data_types.ads
@@ -0,0 +1,27 @@
+package Audio.Wavefiles.Data_Types is
+
+   D_8      : constant := 1.0 / 2.0 ** (8  - 1);
+   D_16     : constant := 1.0 / 2.0 ** (16 - 1);
+   D_24     : constant := 1.0 / 2.0 ** (24 - 1);
+   D_32     : constant := 1.0 / 2.0 ** (32 - 1);
+   D_64     : constant := 1.0 / 2.0 ** (64 - 1);
+
+   type Wav_Fixed_8 is delta D_8 range -1.0 .. 1.0 - D_8
+     with Size => 8;
+   type Wav_Fixed_16 is delta D_16 range -1.0 .. 1.0 - D_16
+     with Size => 16;
+   type Wav_Fixed_24 is delta D_24 range -1.0 .. 1.0 - D_24
+     with Size => 24;
+   type Wav_Fixed_32 is delta D_32 range -1.0 .. 1.0 - D_32
+     with Size => 32;
+   type Wav_Fixed_64 is delta D_64 range -1.0 .. 1.0 - D_64
+     with Size => 64;
+
+   type Wav_Float_32 is digits 6
+     with Size => 32;
+   type Wav_Float_64 is digits 15
+     with Size => 64;
+   type Wav_Float_128 is digits 18
+     with Size => 128;
+
+end Audio.Wavefiles.Data_Types;
