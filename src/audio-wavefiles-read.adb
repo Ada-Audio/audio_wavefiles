2a623581240fce078efd317be22e66b86a3e4844
gusthoff <gusthoff@gusthoff.com>
2020-10-29 03:07:09 +0100

    Using chunk information in Read_Until_Data_Start procedure.
    Replacing manual parsing; using previously retrieved chunk information.
    Introducing auxiliary procedures: Parse_Fmt_Chunk, Parse_Data_Chunk.

--- src/audio-wavefiles-read.adb
+++ src/audio-wavefiles-read.adb
@@ -35,122 +35,143 @@ with Audio.Wavefiles.Internals;    use Audio.Wavefiles.Internals;
 
 package body Audio.Wavefiles.Read is
 
+   procedure Parse_Fmt_Chunk
+     (WF : in out Wavefile);
+   procedure Parse_Data_Chunk
+     (WF : in out Wavefile);
+
+   procedure Parse_Fmt_Chunk
+     (WF : in out Wavefile)
+   is
+      Verbose : constant Boolean := False;
+   begin
+      for Chunk_Header of WF.RIFF_Info.Chunks loop
+
+         if Chunk_Header.Chunk_Tag = Wav_Chunk_Fmt then
+            Ada.Streams.Stream_IO.Set_Index (WF.File, Chunk_Header.File_Index);
+
+            case Chunk_Header.Size is
+            when Wave_Format_Chunk_Size'Enum_Rep (Wave_Format_16_Size) =>
+               Wave_Format_16'Read (WF.File_Access,
+                                    Wave_Format_16 (WF.Wave_Format));
+               Reset_For_Wave_Format_16 (WF.Wave_Format);
+            when Wave_Format_Chunk_Size'Enum_Rep (Wave_Format_18_Size) =>
+               Wave_Format_18'Read (WF.File_Access,
+                                    Wave_Format_18 (WF.Wave_Format));
+               Reset_For_Wave_Format_18 (WF.Wave_Format);
+
+               if Verbose then
+                  Put_Line ("Size of waveformat record "
+                            & Integer'Image (
+                              Wave_Format_18'Value_Size / 8));
+                  Put_Line ("BitsPerSample: "
+                            & Positive'Image
+                              (To_Positive (WF.Wave_Format.Bits_Per_Sample)));
+                  Put_Line ("Size: " & Interfaces.Unsigned_16'Image
+                            (WF.Wave_Format.Size));
+               end if;
+
+            when Wave_Format_Chunk_Size'Enum_Rep
+                 (Wave_Format_Extensible_Size) =>
+               Wave_Format_Extensible'Read (WF.File_Access,
+                                            WF.Wave_Format);
+
+               if Verbose then
+                  Put_Line ("Size of waveformat record "
+                            & Integer'Image
+                              (Wave_Format_Extensible'Value_Size / 8));
+                  Put_Line ("File index: " & Integer'Image (
+                            Integer (Ada.Streams.Stream_IO.Index (WF.File))));
+                  Put_Line ("BitsPerSample: "
+                            & Positive'Image
+                              (To_Positive (WF.Wave_Format.Bits_Per_Sample)));
+                  Put_Line ("Size: " & Interfaces.Unsigned_16'Image
+                            (WF.Wave_Format.Size));
+               end if;
+
+            when others =>
+               raise Wavefile_Error;
+            end case;
+
+            if Verbose then
+               Display_Info (WF);
+               Put_Line ("fmt chunk size: " & Long_Integer'Image
+                         (Chunk_Header.Size));
+            end if;
+
+            exit;
+         end if;
+
+      end loop;
+   end Parse_Fmt_Chunk;
+
+   procedure Parse_Data_Chunk
+     (WF : in out Wavefile)
+   is
+      Verbose : constant Boolean := False;
+
+      use      Ada.Streams;
+      use type Ada.Streams.Stream_IO.Count;
+   begin
+      for Chunk_Header of WF.RIFF_Info.Chunks loop
+
+         if Chunk_Header.Chunk_Tag = Wav_Chunk_Data then
+            Ada.Streams.Stream_IO.Set_Index (WF.File, Chunk_Header.File_Index);
+
+            if Verbose then
+               Put_Line ("RIFF Tag: " & Chunk_Header.ID);
+            end if;
+
+            WF.File_Index := Chunk_Header.File_Index
+              - Stream_IO.Count (RIFF_Chunk_Header'Size / 8);
+
+            WF.Samples := Chunk_Header.Size /
+              (Long_Integer (To_Positive (WF.Wave_Format.Bits_Per_Sample))
+               / 8);
+
+            if Verbose then
+               Put_Line ("Data chunk size: " & Long_Integer'Image
+                         (Chunk_Header.Size));
+               Put_Line ("Num samples: " & Long_Integer'Image (WF.Samples));
+               Put_Line ("Num samples: " & Long_Integer'Image (WF.Samples
+                         / Long_Integer (WF.Wave_Format.Channels)));
+            end if;
+
+            exit;
+         end if;
+
+      end loop;
+   end Parse_Data_Chunk;
+
    procedure Read_Until_Data_Start
      (WF          : in out Wavefile)
    is
-      Chunk_Header : RIFF_Chunk_Header;
       Verbose      : constant Boolean := False;
    begin
-      --  Read/check RIFF Chunk
-      RIFF_Chunk_Header'Read (WF.File_Access, Chunk_Header);
-      if Chunk_Header.ID /= "RIFF" then
-         raise Wavefile_Error;
+      if WF.RIFF_Info.Chunks.Is_Empty then
+         Audio.Wavefiles.Read.Parse_Wav_Chunks (WF);
       end if;
-      if Verbose then
-         Put_Line ("RIFF Tag: " & Chunk_Header.ID);
-         Put_Line ("RIFF/WAVE chunk size: "
-                   & Interfaces.Unsigned_32'Image (Chunk_Header.Size));
-      end if;
-
-      --  Read/check WAVE tag
-      declare
-         ID : FOURCC_String;
-      begin
-         FOURCC_String'Read (WF.File_Access, ID);
-         if ID /= "WAVE" then
-            raise Wavefile_Error;
-         end if;
-         if Verbose then
-            Put_Line ("RIFF Tag: " & ID);
-         end if;
-      end;
-
-      --  Read/skip chunks until fmt chunk
-      loop
-         RIFF_Chunk_Header'Read (WF.File_Access, Chunk_Header);
-         if Verbose then
-            Put_Line ("RIFF Tag: " & Chunk_Header.ID);
-         end if;
-         exit when Chunk_Header.ID = "fmt ";
-         Skip_Bytes (WF.File, Chunk_Header.Size);
-      end loop;
-
-      case Chunk_Header.Size is
-         when Wave_Format_Chunk_Size'Enum_Rep (Wave_Format_16_Size) =>
-            Wave_Format_16'Read (WF.File_Access,
-                                 Wave_Format_16 (WF.Wave_Format));
-            Reset_For_Wave_Format_16 (WF.Wave_Format);
-         when Wave_Format_Chunk_Size'Enum_Rep (Wave_Format_18_Size) =>
-            Wave_Format_18'Read (WF.File_Access,
-                                 Wave_Format_18 (WF.Wave_Format));
-            Reset_For_Wave_Format_18 (WF.Wave_Format);
-
-            if Verbose then
-               Put_Line ("Size of waveformat record "
-                         & Integer'Image (
-                           Wave_Format_18'Value_Size / 8));
-               Put_Line ("BitsPerSample: "
-                         & Positive'Image
-                           (To_Positive (WF.Wave_Format.Bits_Per_Sample)));
-               Put_Line ("Size: " & Interfaces.Unsigned_16'Image
-                         (WF.Wave_Format.Size));
-            end if;
-
-         when Wave_Format_Chunk_Size'Enum_Rep (Wave_Format_Extensible_Size) =>
-            Wave_Format_Extensible'Read (WF.File_Access,
-                                         WF.Wave_Format);
-
-            if Verbose then
-               Put_Line ("Size of waveformat record "
-                         & Integer'Image
-                           (Wave_Format_Extensible'Value_Size / 8));
-               Put_Line ("File index: " & Integer'Image (
-                         Integer (Ada.Streams.Stream_IO.Index (WF.File))));
-               Put_Line ("BitsPerSample: "
-                         & Positive'Image
-                           (To_Positive (WF.Wave_Format.Bits_Per_Sample)));
-               Put_Line ("Size: " & Interfaces.Unsigned_16'Image
-                         (WF.Wave_Format.Size));
-            end if;
-
-         when others =>
-            raise Wavefile_Error;
-      end case;
 
       if Verbose then
-         Display_Info (WF);
-         Put_Line ("fmt chunk size: " & Interfaces.Unsigned_32'Image
-                   (Chunk_Header.Size));
+         Put_Line ("--------------------------------");
+         Put_Line (WF.RIFF_Info.Id'Image);
+         Put_Line (WF.RIFF_Info.Format'Image);
+         Put_Line (WF.RIFF_Info.Chunks.Length'Image);
+         for E of WF.RIFF_Info.Chunks loop
+            Put_Line ("---- Chunk");
+            Put_Line (E.Chunk_Tag'Image);
+            Put_Line (E.ID);
+            Put_Line (E.Size'Image);
+            Put_Line (E.File_Index'Image);
+            Put_Line (E.Consolidated'Image);
+         end loop;
+         Put_Line ("--------------------------------");
       end if;
 
-      --  Read/skip chunks until data chunk
-      loop
-         RIFF_Chunk_Header'Read (WF.File_Access, Chunk_Header);
-         if Verbose then
-            Put_Line ("RIFF Tag: " & Chunk_Header.ID);
-         end if;
-         exit when Chunk_Header.ID = "data";
-         Skip_Bytes (WF.File, Chunk_Header.Size);
-      end loop;
+      Parse_Fmt_Chunk (WF);
 
-      Set_Index_For_Appending : declare
-         use Ada.Streams;
-         use type Stream_IO.Count;
-      begin
-         WF.File_Index := Stream_IO.Index (WF.File) -
-           Stream_IO.Count (Chunk_Header'Size / 8);
-      end Set_Index_For_Appending;
+      Parse_Data_Chunk (WF);
 
-      WF.Samples := Long_Integer (Chunk_Header.Size)
-        / (Long_Integer (To_Positive (WF.Wave_Format.Bits_Per_Sample)) / 8);
-
-      if Verbose then
-         Put_Line ("Data chunk size: " & Interfaces.Unsigned_32'Image
-                   (Chunk_Header.Size));
-         Put_Line ("Num samples: " & Long_Integer'Image (WF.Samples));
-         Put_Line ("Num samples: " & Long_Integer'Image (WF.Samples
-                   / Long_Integer (WF.Wave_Format.Channels)));
-      end if;
    end Read_Until_Data_Start;
 
    procedure Parse_Wav_Chunks
