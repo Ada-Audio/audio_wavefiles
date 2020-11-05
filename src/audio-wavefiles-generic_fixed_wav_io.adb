924b20a2af4b2d2aa512be310430814bfd4be482
gusthoff <gusthoff@gusthoff.com>
2020-11-05 17:22:02 +0100

    Updating both Total and Current counters when writing to wavefiles.
    This change aligns the Current counter with the actual file index.

--- src/audio-wavefiles-generic_fixed_wav_io.adb
+++ src/audio-wavefiles-generic_fixed_wav_io.adb
@@ -124,7 +124,9 @@ package body Audio.Wavefiles.Generic_Fixed_Wav_IO is
          Wav_MC_Sample'Write (WF.File_Access, Wav);
       end if;
 
-      WF.Sample.Total := WF.Sample.Total + 1;
+      WF.Sample := (Total   => WF.Sample.Total   + 1,
+                    Current => WF.Sample.Current + 1);
+
 
       pragma Assert (Ada.Streams.Stream_IO.Index (WF.File) =
                        Prev_File_Index + Expected_Byte_IO);
