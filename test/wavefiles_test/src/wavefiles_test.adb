fb452687bfec0c45fb754958ee01b96c1f392841
gusthoff <gusthoff@gusthoff.com>
2020-12-03 20:46:02 +0100

    Editorial changes: aligning use column

--- test/wavefiles_test/src/wavefiles_test.adb
+++ test/wavefiles_test/src/wavefiles_test.adb
@@ -27,10 +27,10 @@
 --  DEALINGS IN THE SOFTWARE.
 -------------------------------------------------------------------------------
 
-with Ada.Command_Line; use Ada.Command_Line;
-with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Command_Line;    use Ada.Command_Line;
+with Ada.Text_IO;         use Ada.Text_IO;
 
-with Wave_Test_Instances;  use Wave_Test_Instances;
+with Wave_Test_Instances; use Wave_Test_Instances;
 
 procedure Wavefiles_Test is
    Command_Line_OK : Boolean := False;
