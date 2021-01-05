d70ebd9cb8c1d2aad425a4bbb3ac2836e61809b8
gusthoff <gusthoff@gusthoff.com>
2021-01-05 12:40:20 +0100

    Editorial change: updating copyright year.

--- src/audio-wavefiles.ads
+++ src/audio-wavefiles.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --  The MIT License (MIT)                                                   --
 --                                                                          --
---  Copyright (c) 2015 -- 2020 Gustavo A. Hoffmann                          --
+--  Copyright (c) 2015 -- 2021 Gustavo A. Hoffmann                          --
 --                                                                          --
 --  Permission is hereby granted, free of charge, to any person obtaining   --
 --  a copy of this software and associated documentation files (the         --
